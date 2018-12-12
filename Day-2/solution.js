b85b9571c4cb89bde16c69a2d67ee16f7ed26b5b
Bas K <39069678+Basje12@users.noreply.github.com>
2018-12-12 21:12:10 +0100

    Day 2 Part 2

--- Day-2/solution.js
+++ Day-2/solution.js
@@ -20,3 +20,29 @@ input.forEach(arr => {
 console.log('Answer part 1: ', checkSum);
 
 // Part 2
+let checkDivisible = 0;
+input.forEach(arr => {
+  let check = arr
+    .split('\t')
+    .map(item => {
+      let num = Number(item);
+      return num;
+    })
+    .sort((a, b) => (a > b ? 1 : -1));
+
+  for (let i = 0; i < check.length; i++) {
+    for (let j = 0; j < check.length; j++) {
+      if (i !== j) {
+        let num1 = check[i];
+        let num2 = check[j];
+
+        if ((num1 / num2) % 1 === 0) {
+          checkDivisible += num1 / num2;
+          break;
+        }
+      }
+    }
+  }
+});
+
+console.log('Answer part 2: ', checkDivisible);
